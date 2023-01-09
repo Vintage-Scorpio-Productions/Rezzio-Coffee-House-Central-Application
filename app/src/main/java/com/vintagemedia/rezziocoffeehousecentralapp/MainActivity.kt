97ee289bb2af393e6023a89e8962b227e4d717eb
borkusgod <lightraven.bourque@gmail.com>
2023-01-09 11:52:42 -0400

    Created the color palette and worked it into the theme. still have the type and shapes to go over and make sure they're right.

--- app/src/main/java/com/vintagemedia/rezziocoffeehousecentralapp/MainActivity.kt
+++ app/src/main/java/com/vintagemedia/rezziocoffeehousecentralapp/MainActivity.kt
@@ -6,20 +6,10 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.vintagemedia.rezziocoffeehousecentralapp.devUtilities.DefaultPlaceholder
 import com.vintagemedia.rezziocoffeehousecentralapp.ui.theme.RezzioCoffeeHouseCentralAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,40 +18,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             RezzioCoffeeHouseCentralAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     // below is for defaulting to landscape
                     val activity = (LocalContext.current as Activity)
                     activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-
+                    // from util
                     DefaultPlaceholder()
                 }
             }
         }
     }
-}
 
-@Composable
-fun DefaultPlaceholder() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.LightGray)
-            .padding(1.dp)
-            .border(5.dp, MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(5))
-        ,
-        contentAlignment = Alignment.Center
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxHeight()
-                .padding(5.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "Placeholder")
-        }
-    }
-}
+
 
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Preview(showBackground = true)
